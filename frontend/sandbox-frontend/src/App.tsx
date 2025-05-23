2140e6fd9fda9bfdc8e7c5e2490bdaa2c0a2490c
Hideki_Yoshikura <evenintherain@gmail.com>
2025-05-23 21:49:54 +0900

    Chapter1:Auth0認証の実装

--- frontend/sandbox-frontend/src/App.tsx
+++ frontend/sandbox-frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    const {
+    user,
+    logout,
+    loginWithRedirect,
+    isAuthenticated,
+    isLoading,
+    } = useAuth0();
+    const onClickLoginButton = () => {
+      loginWithRedirect();
+    };
+    const onClickLogoutButton = () => {
+      logout();
+    };
+    return (
+    <div className="App">
+        <header className="App-header">
+        <button
+            type="button"
+            onClick={isAuthenticated ? onClickLogoutButton : onClickLoginButton}
+        >
+        {isAuthenticated ? 'logout' : 'login'}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+          <div>{isLoading ? 'LoadingNow' : (user?.name ?? 'unauthorized')}</div>
+        </header>
+    </div>
+    );
 }
-
-export default App
+export default App;            
