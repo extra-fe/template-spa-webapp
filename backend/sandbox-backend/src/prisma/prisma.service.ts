3bb4d55a14fdc74488fdd3440feb55aa49b5747d
Hideki_Yoshikura <evenintherain@gmail.com>
2025-10-19 11:18:29 +0900

    Prismaのクエリログ追加

--- backend/sandbox-backend/src/prisma/prisma.service.ts
+++ backend/sandbox-backend/src/prisma/prisma.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@nestjs/common/decorators';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: process.env.PRISMA_LOG_LEVEL?.split(',') as any,
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
