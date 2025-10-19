b7bd9e927e0ab478d456d1e8593a7dc47787a16f
Hideki_Yoshikura <evenintherain@gmail.com>
2025-10-19 17:26:25 +0900

    変数とセキュリティグループの追加

--- iac/aws/variables.tf
+++ iac/aws/variables.tf
@@ -83,3 +83,9 @@ variable "api-expose-port" {
   default     = 3000
   description = "DockerfileでEXPOSEしているポートを指定"
 }
+
+variable "local-pc-ip-addresses" {
+  type        = list(string)
+  default     = []
+  description = "接続元のIPアドレス"
+}
