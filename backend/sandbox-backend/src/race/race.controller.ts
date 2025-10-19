da42e0cdd8362f8bd2f7b4f85590ff2857866cd0
Hideki_Yoshikura <evenintherain@gmail.com>
2025-10-19 18:35:25 +0900

    raceにguard追加

--- backend/sandbox-backend/src/race/race.controller.ts
+++ backend/sandbox-backend/src/race/race.controller.ts
@@ -1,20 +1,25 @@
 import { RaceService } from './race.service';
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post,UseGuards  } from '@nestjs/common';
 import { CreateRaceDto } from './dto/create-race.dto';
+import { ConditionalAuthGuard } from 'src/auth/guards/conditional-auth.guard';
 
 @Controller('api/races')
 export class RaceController {
   constructor(private readonly raceService: RaceService) {}
 
+  @UseGuards(ConditionalAuthGuard)
   @Get()
   findAll() {
     return this.raceService.findAll();
   }
 
+  @UseGuards(ConditionalAuthGuard)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.raceService.findOne(id);
   }
+
+  @UseGuards(ConditionalAuthGuard)
   @Post()
   create(@Body() createRaceDto: CreateRaceDto) {
     return this.raceService.create(createRaceDto);
