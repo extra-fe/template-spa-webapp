08632a4cc68b861ed5646f8603f6088759d0d79b
Hideki_Yoshikura <evenintherain@gmail.com>
2025-11-08 16:29:52 +0900

    クエリのログ追加

--- backend/sandbox-backend/src/prisma/prisma.service.ts
+++ backend/sandbox-backend/src/prisma/prisma.service.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: process.env.PRISMA_LOG_LEVEL?.split(',') as any,
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
