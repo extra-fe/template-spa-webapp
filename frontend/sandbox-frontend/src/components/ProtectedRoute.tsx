cb387dbebf91183201e2261815dd2999af9cffbc
Hideki_Yoshikura <evenintherain@gmail.com>
2025-11-08 20:25:55 +0900

    認証状態を検証するコンポーネント作成

---
+++ frontend/sandbox-frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import type { JSX } from 'react';
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!isAuthenticated) {
+    loginWithRedirect(); // 認証してないならリダイレクト
+    return null;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
